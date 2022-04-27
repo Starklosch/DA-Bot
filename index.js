18979c20035864b66611d1d45582f7ee60695fd1
Starklosch <45690579+Starklosch@users.noreply.github.com>
2022-04-27 15:15:47 -0300

    Errors fixed

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const ds = require("discord.js")
-const client = new ds.Client()
- 
-client.on("ready", console.log("I'm here!"))
+require('dotenv').config();
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS] });
+
+client.login(process.env.CLIENT_TOKEN)
+
+client.on("ready", () => {
+    console.log("I'm here!")
+})
 
 client.on("message", async msg => {
     if (msg.content.match(/ping/i)){
